869ac4f68ac0ac641016448921ae56b06c6fa6a4
Brad Bohen <bbohen@gmail.com>
2018-04-16 23:15:44 -0400

    Fixed example

--- example/example.js
+++ example/example.js
@@ -9,49 +9,7 @@ function exampleUsage() {
   const randomValuesLength = 16;
   const randomValues = createRandomValues(randomValuesLength, chartHeight);
 
-  const dev_tool_values = [
-    13.5138,
-    13.49,
-    13.355,
-    13.275,
-    13.305,
-    13.32,
-    13.28,
-    13.245,
-    13.2448,
-    13.3036,
-    13.335,
-    13.325,
-    13.425,
-    13.3933,
-    13.3942,
-    13.39,
-    13.4333,
-    13.4,
-    13.367,
-    13.385,
-    13.33,
-    13.365,
-    13.3501,
-    13.355,
-    13.3549,
-    13.335,
-    13.2989,
-    13.3,
-    13.2969,
-    13.275,
-    13.2708,
-    13.305,
-    13.27,
-    13.265,
-    13.25,
-    13.305,
-    13.285,
-    13.2932,
-    13.32
-  ];
-
-  chart(dev_tool_values, 8000, 10, chartId);
+  chart(randomValues, chartHeight, 50, chartId);
 }
 
 window.onload = exampleUsage;
